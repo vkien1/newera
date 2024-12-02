cd7eca6820ef03cfe236d90679ec10c97c94342c
Kien Vu <officialkienv@gmail.com>
2024-12-02 12:44:46 -0500

    cleaned up , added task manager frontend

--- app.py
+++ app.py
@@ -192,3 +192,10 @@ if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
+
+# Add Task
+
+# Delete Task
+
+# Edit Task
+
