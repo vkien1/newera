3f8ac6a464b874ec93d8a0c94ab465244a1f1b49
Kien Vu <officialkienv@gmail.com>
2024-12-02 12:09:03 -0500

    Revert "added tasks, categories tables"
    This reverts commit a0eb34e03b294e76af739aefe4c448b19bd59f30.

--- database/database_setup.py
+++ database/database_setup.py
@@ -15,34 +15,6 @@ cursor.execute('''
     )
 ''')
 
-# Create the tasks table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER NOT NULL,
-        title TEXT NOT NULL,
-        description TEXT,
-        due_date DATE,
-        send_alert BOOLEAN DEFAULT FALSE,
-        priority INT,
-        status TEXT,
-        CONSTRAINT fk_user
-            FOREIGN KEY (user_id)
-            REFERENCES users(id)           
-    )
-''')
-
-# Create the categories table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS categories
-        id INTEGER PRIMARY KEY AUTOINCREMENT
-        task_id INTEGER NOT NULL
-        name TEXT
-        CONSTRAINT fk_task
-            FOREIGN KEY (task_id)
-            REFERENCES tasks(id)
-''')
-
 # Commit the changes and close the connection
 conn.commit()
 conn.close()
